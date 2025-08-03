b90a10279ddd7d2d44c250c3b1245946f3bda935
sbsmrth <andresf.mrh@gmail.com>
2025-08-03 09:39:41 -0500

    feat: env variables setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('PaymentsMS');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(envs.port);
+
+  logger.log(`Payments Microservice is running on port ${envs.port}`);
 }
 bootstrap();
